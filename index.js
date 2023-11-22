8540e0246c680c97dcf40b0ea6a367e849673f8c
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-22 21:41:23 +0600

    feature part updated

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function run() {
 
     //food update
     app.put('/updatefood/:foodid',async(req,res)=>{
-      const id =req.params.id;
+      const id =req.params.foodid;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateFood= req.body;
